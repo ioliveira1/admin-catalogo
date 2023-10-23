7c41b45dd878600fcf766ab1b68ffbaea18ade9f
Igor Oliveira <igorfoliveira@gmail.com>
2023-10-23 18:05:49 -0300

    importando o mockito

--- application/src/test/java/com/ioliveira/admin/catalogo/application/UseCaseUnitTest.java
+++ application/src/test/java/com/ioliveira/admin/catalogo/application/UseCaseUnitTest.java
@@ -1,7 +1,10 @@
 package com.ioliveira.admin.catalogo.application;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class UseCaseUnitTest {
 
     @Test
