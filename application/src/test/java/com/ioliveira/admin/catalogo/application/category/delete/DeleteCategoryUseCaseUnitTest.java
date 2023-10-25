c71a04cea0e2abba26fb25fd53a66131b5d105ff
Igor Oliveira <igorfoliveira@gmail.com>
2023-10-25 14:57:11 -0300

    testes unitarios do caso de uso de delete de categoria

---
+++ application/src/test/java/com/ioliveira/admin/catalogo/application/category/delete/DeleteCategoryUseCaseUnitTest.java
@@ -0,0 +1,72 @@
+package com.ioliveira.admin.catalogo.application.category.delete;
+
+import com.ioliveira.admin.catalogo.domain.category.Category;
+import com.ioliveira.admin.catalogo.domain.category.CategoryGateway;
+import com.ioliveira.admin.catalogo.domain.category.CategoryID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class DeleteCategoryUseCaseUnitTest {
+
+    @InjectMocks
+    private DefaultDeleteCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @BeforeEach
+    void cleanUp() {
+        reset(categoryGateway);
+    }
+
+    @Test
+    public void givenAValidId_whenCallsDeleteCategoryUseCase_shouldBeOk() {
+        final Category category = Category.newCategory("Filmes", "A categoria mais assistida", true);
+        final CategoryID expectedId = category.getId();
+
+        doNothing().when(categoryGateway).deleteById(eq(expectedId));
+
+        assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        verify(categoryGateway, times(1)).deleteById(eq(expectedId));
+    }
+
+    @Test
+    public void givenAnInvalidId_whenCallsDeleteCategoryUseCase_shouldBeOk() {
+        final CategoryID expectedId = CategoryID.from("invalid");
+
+        doNothing().when(categoryGateway).deleteById(eq(expectedId));
+
+        assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        verify(categoryGateway, times(1)).deleteById(eq(expectedId));
+    }
+
+    @Test
+    public void givenAValidId_whenGatewayThrowsError_shouldReturnException() {
+        final Category category = Category.newCategory("Filmes", "A categoria mais assistida", true);
+        final CategoryID expectedId = category.getId();
+
+        doThrow(new IllegalStateException("Gateway error"))
+                .when(categoryGateway).deleteById(eq(expectedId));
+
+        assertThrows(IllegalAccessError.class, () -> useCase.execute(expectedId.getValue()));
+
+        verify(categoryGateway, times(1)).deleteById(eq(expectedId));
+    }
+
+}
