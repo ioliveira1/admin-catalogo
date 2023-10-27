1824682861fe48502d5c02f15928c4c12af8d415
Igor Oliveira <igorfoliveira@gmail.com>
2023-10-27 16:21:34 -0300

    adicionando springboot na camada infrastructure

--- infrastructure/src/main/java/com/ioliveira/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/ioliveira/admin/catalogo/infrastructure/Main.java
@@ -1,7 +1,12 @@
 package com.ioliveira.admin.catalogo.infrastructure;
 
+import com.ioliveira.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infrastructure/src/main/java/com/ioliveira/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.ioliveira.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ioliveira.admin.catalogo")
+public class WebServerConfig {
+}
