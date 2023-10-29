1e38fa88d7843a865aca55838eedaeec91fa5b08
Igor Oliveira <igorfoliveira@gmail.com>
2023-10-28 21:12:37 -0300

    criando a implementacao do gateway de categoria

---
+++ infrastructure/src/main/java/com/ioliveira/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
@@ -0,0 +1,47 @@
+package com.ioliveira.admin.catalogo.infrastructure.category;
+
+import com.ioliveira.admin.catalogo.domain.category.Category;
+import com.ioliveira.admin.catalogo.domain.category.CategoryGateway;
+import com.ioliveira.admin.catalogo.domain.category.CategoryID;
+import com.ioliveira.admin.catalogo.domain.category.CategorySearchQuery;
+import com.ioliveira.admin.catalogo.domain.pagination.Pagination;
+import com.ioliveira.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class CategoryMySQLGateway implements CategoryGateway {
+
+    private final CategoryRepository repository;
+
+    public CategoryMySQLGateway(final CategoryRepository repository) {
+        this.repository = Objects.requireNonNull(repository);
+    }
+
+    @Override
+    public Category create(final Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(final CategoryID id) {
+
+    }
+
+    @Override
+    public Optional<Category> findById(final CategoryID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Category update(final Category category) {
+        return null;
+    }
+
+    @Override
+    public Pagination<Category> findAll(final CategorySearchQuery query) {
+        return null;
+    }
+}
