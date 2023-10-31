a37ad06088a8fe466b87aeeda6048c8bb03ea1be
Igor Oliveira <igorfoliveira@gmail.com>
2023-10-31 12:19:19 -0300

    teste integrado do caso de uso de get de categoria por id

---
+++ infrastructure/src/test/java/com/ioliveira/admin/catalogo/application/category/retrieve/get/GetCategoryByIdUseCaseIT.java
@@ -0,0 +1,59 @@
+package com.ioliveira.admin.catalogo.application.category.retrieve.get;
+
+import com.ioliveira.admin.catalogo.IntegrationTest;
+import com.ioliveira.admin.catalogo.domain.category.Category;
+import com.ioliveira.admin.catalogo.domain.category.CategoryID;
+import com.ioliveira.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
+import com.ioliveira.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@IntegrationTest
+public class GetCategoryByIdUseCaseIT {
+
+    @Autowired
+    private GetCategoryByIdUseCase useCase;
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @BeforeEach
+    void cleanUp() {
+        this.repository.deleteAll();
+    }
+
+    @Test
+    public void givenAValidId_whenCallsGetCategoryUseCase_shouldReturnCategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final Category category = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final CategoryID expectedId = category.getId();
+
+        assertEquals(0, repository.count());
+
+        repository.saveAndFlush(CategoryJpaEntity.from(category));
+
+        assertEquals(1, repository.count());
+
+        final var output = useCase.execute(expectedId.getValue());
+
+        final DateTimeFormatter formatter =
+                new DateTimeFormatterBuilder().appendInstant(6).toFormatter();
+
+        assertEquals(expectedId, output.id());
+        assertEquals(expectedName, output.name());
+        assertEquals(expectedDescription, output.description());
+        assertEquals(expectedIsActive, output.isActive());
+        assertEquals(formatter.format(category.getCreatedAt()), formatter.format(output.createdAt()));
+        assertEquals(formatter.format(category.getUpdatedAt()), formatter.format(output.updatedAt()));
+        assertEquals(category.getDeletedAt(), output.deletedAt());
+    }
+}
