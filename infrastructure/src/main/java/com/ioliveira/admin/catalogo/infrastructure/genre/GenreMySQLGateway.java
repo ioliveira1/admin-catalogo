64dea0da25284df418de89488524f965b2b9fcd5
Igor Oliveira <igorfoliveira@gmail.com>
2023-11-09 12:43:43 -0300

    implementando o metodo create do gateway de genre

--- infrastructure/src/main/java/com/ioliveira/admin/catalogo/infrastructure/genre/GenreMySQLGateway.java
+++ infrastructure/src/main/java/com/ioliveira/admin/catalogo/infrastructure/genre/GenreMySQLGateway.java
@@ -5,16 +5,27 @@ import com.ioliveira.admin.catalogo.domain.genre.GenreGateway;
 import com.ioliveira.admin.catalogo.domain.genre.GenreID;
 import com.ioliveira.admin.catalogo.domain.pagination.Pagination;
 import com.ioliveira.admin.catalogo.domain.pagination.SearchQuery;
+import com.ioliveira.admin.catalogo.infrastructure.genre.persistence.GenreJpaEntity;
+import com.ioliveira.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Component
 public class GenreMySQLGateway implements GenreGateway {
 
+    private final GenreRepository genreRepository;
+
+    public GenreMySQLGateway(final GenreRepository genreRepository) {
+        this.genreRepository = Objects.requireNonNull(genreRepository);
+    }
+
     @Override
     public Genre create(final Genre genre) {
-        return null;
+        return this.genreRepository
+                .save(GenreJpaEntity.from(genre))
+                .toAggregate();
     }
 
     @Override
