2aeac878c0f1e9f27f13139c5ef177c4a7ba6d57
Igor Oliveira <igorfoliveira@gmail.com>
2023-11-10 19:18:02 -0300

    teste integrado do caso de uso de delete de genero

---
+++ infrastructure/src/test/java/com/ioliveira/admin/catalogo/application/genre/delete/DeleteGenreUseCaseIT.java
@@ -0,0 +1,39 @@
+package com.ioliveira.admin.catalogo.application.genre.delete;
+
+import com.ioliveira.admin.catalogo.IntegrationTest;
+import com.ioliveira.admin.catalogo.domain.genre.Genre;
+import com.ioliveira.admin.catalogo.domain.genre.GenreGateway;
+import com.ioliveira.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@IntegrationTest
+public class DeleteGenreUseCaseIT {
+
+    @Autowired
+    private DeleteGenreUseCase useCase;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Autowired
+    private GenreGateway genreGateway;
+
+    @Test
+    public void givenAValidGenreId_whenCallsDeleteGenre_shouldDeleteGenre() {
+        assertEquals(0, genreRepository.count());
+
+        final Genre genre = genreGateway.create(Genre.newGenre("Ação", true));
+
+        assertEquals(1, genreRepository.count());
+
+        final var expectedId = genre.getId();
+
+        useCase.execute(expectedId.getValue());
+
+        assertEquals(0, genreRepository.count());
+    }
+
+}
