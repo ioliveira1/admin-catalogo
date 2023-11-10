bd118fce6e2261319bff83c00b8efb1402a8b8dd
Igor Oliveira <igorfoliveira@gmail.com>
2023-11-10 19:40:08 -0300

    teste integrado do caso de uso de listagem de generos

---
+++ infrastructure/src/test/java/com/ioliveira/admin/catalogo/application/genre/retrieve/list/ListGenreUseCaseIT.java
@@ -0,0 +1,66 @@
+package com.ioliveira.admin.catalogo.application.genre.retrieve.list;
+
+import com.ioliveira.admin.catalogo.IntegrationTest;
+import com.ioliveira.admin.catalogo.application.genre.retreieve.list.GenreListOutput;
+import com.ioliveira.admin.catalogo.application.genre.retreieve.list.ListGenreUseCase;
+import com.ioliveira.admin.catalogo.domain.genre.Genre;
+import com.ioliveira.admin.catalogo.domain.pagination.SearchQuery;
+import com.ioliveira.admin.catalogo.infrastructure.genre.persistence.GenreJpaEntity;
+import com.ioliveira.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@IntegrationTest
+public class ListGenreUseCaseIT {
+
+    @Autowired
+    private ListGenreUseCase useCase;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Test
+    public void givenAValidQuery_whenCallsListGenre_shouldReturnGenres() {
+        final var genres = List.of(
+                Genre.newGenre("Ação", true),
+                Genre.newGenre("Aventura", true)
+        );
+
+        genreRepository.saveAllAndFlush(
+                genres.stream().map(GenreJpaEntity::from).toList()
+        );
+
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "A";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+        final var expectedTotal = 2;
+
+        final var expectedItems = genres.stream()
+                .map(GenreListOutput::from)
+                .toList();
+
+        final var query =
+                new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        final var output = useCase.execute(query);
+
+        assertEquals(expectedPage, output.currentPage());
+        assertEquals(expectedPerPage, output.perPage());
+        assertEquals(expectedTotal, output.total());
+        assertEquals(sorted(expectedItems), sorted(output.items()));
+    }
+
+    private List<GenreListOutput> sorted(final List<GenreListOutput> items) {
+        return items.stream()
+                .sorted(Comparator.comparing(GenreListOutput::name))
+                .toList();
+    }
+
+}
