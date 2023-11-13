70d47217955740208bf52c646f2e3222025e928a
Igor Oliveira <igorfoliveira@gmail.com>
2023-11-13 12:35:31 -0300

    TDD das validacoes de castmember

--- domain/src/test/java/com/ioliveira/admin/catalogo/domain/castmember/CastMemberTest.java
+++ domain/src/test/java/com/ioliveira/admin/catalogo/domain/castmember/CastMemberTest.java
@@ -1,9 +1,11 @@
 package com.ioliveira.admin.catalogo.domain.castmember;
 
+import com.ioliveira.admin.catalogo.domain.exceptions.NotificationException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CastMemberTest {
 
@@ -12,14 +14,86 @@ public class CastMemberTest {
         final var expectedName = "Vin Diesel";
         final var expectedType = CastMemberType.ACTOR;
 
-        final var actualMember = CastMember.newMember(expectedName, expectedType);
+        final var castMember = CastMember.newMember(expectedName, expectedType);
 
-        assertNotNull(actualMember);
-        assertNotNull(actualMember.getId());
-        assertEquals(expectedName, actualMember.getName());
-        assertEquals(expectedType, actualMember.getType());
-        assertNotNull(actualMember.getCreatedAt());
-        assertNotNull(actualMember.getUpdatedAt());
+        assertNotNull(castMember);
+        assertNotNull(castMember.getId());
+        assertEquals(expectedName, castMember.getName());
+        assertEquals(expectedType, castMember.getType());
+        assertNotNull(castMember.getCreatedAt());
+        assertNotNull(castMember.getUpdatedAt());
     }
 
+    @Test
+    public void givenAInvalidNullName_whenCallsNewMember_shouldReceiveANotification() {
+        final String expectedName = null;
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var exception = assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType)
+        );
+
+        assertNotNull(exception);
+        assertEquals(expectedErrorCount, exception.getErrors().size());
+        assertEquals(expectedErrorMessage, exception.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAInvalidEmptyName_whenCallsNewMember_shouldReceiveANotification() {
+        final var expectedName = " ";
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var exception = assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType)
+        );
+
+        assertNotNull(exception);
+        assertEquals(expectedErrorCount, exception.getErrors().size());
+        assertEquals(expectedErrorMessage, exception.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAInvalidNameWithLengthMoreThan255_whenCallsNewMember_shouldReceiveANotification() {
+        final var expectedName = """
+                Gostaria de enfatizar que o consenso sobre a necessidade de qualificação auxilia a preparação e a
+                composição das posturas dos órgãos dirigentes com relação às suas atribuições.
+                Do mesmo modo, a estrutura atual da organização apresenta tendências no sentido de aprovar a
+                manutenção das novas proposições.
+                """;
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+
+        final var exception = assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType)
+        );
+
+        assertNotNull(exception);
+        assertEquals(expectedErrorCount, exception.getErrors().size());
+        assertEquals(expectedErrorMessage, exception.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAInvalidNullType_whenCallsNewMember_shouldReceiveANotification() {
+        final var expectedName = "Vin Diesel";
+        final CastMemberType expectedType = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'type' should not be null";
+
+        final var exception = assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType)
+        );
+
+        assertNotNull(exception);
+        assertEquals(expectedErrorCount, exception.getErrors().size());
+        assertEquals(expectedErrorMessage, exception.getErrors().get(0).message());
+    }
 }
