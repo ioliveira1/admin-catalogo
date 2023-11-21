49e3bc79acab75ec6d9e032b2af243076a7a1698
Igor Oliveira <igorfoliveira@gmail.com>
2023-11-21 19:25:50 -0300

    configurando o keycloak jwt converter

--- infrastructure/src/main/java/com/ioliveira/admin/catalogo/infrastructure/configuration/SecurityConfig.java
+++ infrastructure/src/main/java/com/ioliveira/admin/catalogo/infrastructure/configuration/SecurityConfig.java
@@ -1,22 +1,39 @@
 package com.ioliveira.admin.catalogo.infrastructure.configuration;
 
+import com.nimbusds.jose.shaded.json.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
-    private static final String ROLE_ADMIN = "catalogo_admin";
-    private static final String ROLE_CAST_MEMBERS = "catalogo_cast_members";
-    private static final String ROLE_CATEGORIES = "catalogo_categories";
-    private static final String ROLE_GENRES = "catalogo_genres";
+    private static final String ROLE_ADMIN = "CATALOGO_ADMIN";
+    private static final String ROLE_CAST_MEMBERS = "CATALOGO_CAST_MEMBERS";
+    private static final String ROLE_CATEGORIES = "CATALOGO_CATEGORIES";
+    private static final String ROLE_GENRES = "CATALOGO_GENRES";
 
     @Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
@@ -37,7 +54,8 @@ public class SecurityConfig {
 
         httpSecurity
                 .oauth2ResourceServer()
-                .jwt();
+                .jwt()
+                .jwtAuthenticationConverter(new KeycloakJwtConverter());
 
         httpSecurity
                 .sessionManagement()
@@ -49,6 +67,77 @@ public class SecurityConfig {
                 .sameOrigin();
 
         return httpSecurity.build();
+    }
+
+    static class KeycloakJwtConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+        private final KeycloakAuthoritiesConverter authoritiesConverter;
+
+        public KeycloakJwtConverter() {
+            this.authoritiesConverter = new KeycloakAuthoritiesConverter();
+        }
+
+        @Override
+        public AbstractAuthenticationToken convert(final Jwt jwt) {
+            return new JwtAuthenticationToken(jwt, extractAuthorities(jwt), extractPrincipal(jwt));
+        }
+
+        private String extractPrincipal(final Jwt jwt) {
+            return jwt.getClaimAsString(JwtClaimNames.SUB);
+        }
+
+        private Collection<? extends GrantedAuthority> extractAuthorities(final Jwt jwt) {
+            return this.authoritiesConverter.convert(jwt);
+        }
+    }
+
+    static class KeycloakAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+        private static final String REALM_ACCESS = "realm_access";
+        private static final String ROLES = "roles";
+        private static final String RESOURCE_ACCESS = "resource_access";
+        public static final String SEPARATOR = "_";
+
+        @Override
+        public Collection<GrantedAuthority> convert(final Jwt jwt) {
+            final Stream<String> realmRoles = extractRealmRoles(jwt);
+            final Stream<String> resourceRoles = extractResourceRoles(jwt);
+
+            return Stream.concat(realmRoles, resourceRoles)
+                    .map(role -> new SimpleGrantedAuthority(role.toUpperCase()))
+                    .collect(Collectors.toSet());
+        }
+
+        private Stream<String> extractRealmRoles(final Jwt jwt) {
+            return Optional.ofNullable(jwt.getClaimAsMap(REALM_ACCESS))
+                    .map(resource -> (Collection<String>) resource.get(ROLES))
+                    .orElse(Collections.emptyList())
+                    .stream();
+        }
+
+        private Stream<String> extractResourceRoles(final Jwt jwt) {
+
+            final Function<Map.Entry<String, Object>, Stream<String>> mapResource =
+                    resource -> {
+                        final String key = resource.getKey();
+                        final JSONObject value = (JSONObject) resource.getValue();
+                        final Collection<String> roles = (Collection<String>) value.get(ROLES);
+                        return roles.stream().map(role -> key.concat(SEPARATOR).concat(role));
+                    };
+
+
+            final Function<Set<Map.Entry<String, Object>>, Collection<String>> mapResources =
+                    resource -> resource.stream()
+                            .flatMap(mapResource)
+                            .toList();
+
+
+            return Optional.ofNullable(jwt.getClaimAsMap(RESOURCE_ACCESS))
+                    .map(Map::entrySet)
+                    .map(mapResources)
+                    .orElse(Collections.emptyList())
+                    .stream();
+        }
     }
 
 }
