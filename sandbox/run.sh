16c32187c5ae3c8df2c8d1ec64bd6fbd59619799
Igor Oliveira <igorfoliveira@gmail.com>
2023-11-24 19:26:06 -0300

    adicionando o elasticsearch e a docker network elastic

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,9 +1,13 @@
 docker network create adm_videos_services
+docker network create elastic
 
 mkdir -m 777 .docker
 mkdir -m 777 .docker/keycloak
+mkdir -m 777 .docker/es01
 
 docker compose -f services/docker-compose.yml up -d
+docker compose -f elk/docker-compose.yml up -d
+#docker compose -f app/docker-compose.yml up -d
 
 echo "Inicializando os containers..."
 sleep 20
